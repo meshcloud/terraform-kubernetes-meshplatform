6f67acedb29bb2da60e3c2caccc699f1ddc60de9
Felix Zieger <fzieger@meshcloud.io>
2022-11-17 17:40:56 +0100

    terraform fmt

--- modules/meshcloud-service-account-meshfed-metering/outputs.tf
+++ modules/meshcloud-service-account-meshfed-metering/outputs.tf
@@ -1,21 +1,21 @@
 data "kubernetes_secret" "meshfed_metering_secret" {
   metadata {
-    name = "${kubernetes_service_account.meshfed_metering.default_secret_name}"
-    namespace = "${kubernetes_service_account.meshfed_metering.metadata.0.namespace}"
+    name      = kubernetes_service_account.meshfed_metering.default_secret_name
+    namespace = kubernetes_service_account.meshfed_metering.metadata.0.namespace
   }
-    depends_on = [
+  depends_on = [
     kubernetes_service_account.meshfed_metering
   ]
 }
 
 output "meshfed_metering_token" {
   sensitive = true
-  value = data.kubernetes_secret.meshfed_metering_secret
+  value     = data.kubernetes_secret.meshfed_metering_secret
 }
 
 output "token_metering" {
   sensitive = true
-  value = "${data.kubernetes_secret.meshfed_metering_secret.data["token"]}"
+  value     = data.kubernetes_secret.meshfed_metering_secret.data["token"]
 }
 
 output "expose_token" {
--- modules/meshcloud-service-account-meshfed-replicator/outputs.tf
+++ modules/meshcloud-service-account-meshfed-replicator/outputs.tf
@@ -1,21 +1,21 @@
 data "kubernetes_secret" "meshfed_service_secret" {
   metadata {
-    name = "${kubernetes_service_account.meshfed_service.default_secret_name}"
-    namespace = "${kubernetes_service_account.meshfed_service.metadata.0.namespace}"
+    name      = kubernetes_service_account.meshfed_service.default_secret_name
+    namespace = kubernetes_service_account.meshfed_service.metadata.0.namespace
   }
-    depends_on = [
+  depends_on = [
     kubernetes_service_account.meshfed_service
   ]
 }
 
 output "meshfed_replicator_token" {
   sensitive = true
-  value = data.kubernetes_secret.meshfed_service_secret
+  value     = data.kubernetes_secret.meshfed_service_secret
 }
 
 output "token_replicator" {
   sensitive = true
-  value = "${data.kubernetes_secret.meshfed_service_secret.data["token"]}"
+  value     = data.kubernetes_secret.meshfed_service_secret.data["token"]
 }
 
 output "expose_token" {
