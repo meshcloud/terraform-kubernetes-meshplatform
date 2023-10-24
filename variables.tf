e8e901c18775741d209404a11867c0511ed2b149
Mohammad Alhussan <malhussan@meshcloud.io>
2023-10-24 13:22:01 +0200

    feat: support adding additional rules to the clusterrole

--- variables.tf
+++ variables.tf
@@ -3,7 +3,29 @@ variable "metering_enabled" {
   default = true
 }
 
+variable "metering_additional_rules" {
+  type = list(object({
+    api_groups        = list(string)
+    resources         = list(string)
+    verbs             = list(string)
+    resource_names    = optional(list(string))
+    non_resource_urls = optional(list(string))
+  }))
+  default = []
+}
+
 variable "replicator_enabled" {
   type    = bool
   default = true
+}
+
+variable "replicator_additional_rules" {
+  type = list(object({
+    api_groups        = list(string)
+    resources         = list(string)
+    verbs             = list(string)
+    resource_names    = optional(list(string))
+    non_resource_urls = optional(list(string))
+  }))
+  default = []
 }
